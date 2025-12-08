872307704fece5f090fc3e747ea20e289782d593
Aartisuthar7850 <codewebworld7976@gmail.com>
2025-12-08 20:31:13 +0530

    Time: 8 ms (3.91%) | Memory: 7.7 MB (96.62%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int sumsqofdigits(int n) {
+        int sum = 0;
+        while (n) {
+            sum = sum + ((n % 10) * (n % 10));
+            n /= 10;
+        }
+        return sum;
+    }
+    bool isHappy(int n) {
+        while (n > 4) {
+            n = sumsqofdigits(n);
+            cout << n << "/n";
+        }
+        if (n == 1)
+            return true;
+        else
+            return false;
+    }
+};
