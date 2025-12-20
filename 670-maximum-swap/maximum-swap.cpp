32310dd357ffca81c77d87bba67f96e2524dc8c5
Aartisuthar7850 <codewebworld7976@gmail.com>
2025-12-20 22:10:00 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (59.50%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -2,26 +2,20 @@ class Solution {
 public:
     int maximumSwap(int num) {
         string s = to_string(num);
-        int n = s.size();
-
-        // last position of each digit (0–9)
-        int last[10];
-        for (int i = 0; i < n; i++)
+        int last[10] = {};
+        
+        for (int i = 0; i < s.size(); i++)
             last[s[i] - '0'] = i;
 
-        for (int i = 0; i < n; i++) {
-            // try bigger digits from 9 to current+1
-            for (int d = 9; d > s[i] - '0'; d--) {
+        for (int i = 0; i < s.size(); i++)
+            for (int d = 9; d > s[i] - '0'; d--)
                 if (last[d] > i) {
-                    // XOR swap (no 3rd variable)
                     s[i] ^= s[last[d]];
                     s[last[d]] ^= s[i];
                     s[i] ^= s[last[d]];
-                    
                     return stoi(s);
                 }
-            }
-        }
+
         return num;
     }
 };
